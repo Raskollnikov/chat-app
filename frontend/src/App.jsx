5a9b6048380bc2bdfd40a6d10abbcfed28039002
Raskollnikov <bakhalaia56@gmail.com>
2025-01-09 20:29:24 +0400

    themes added, and settings page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,11 @@ import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
 import {Loader} from 'lucide-react'
 import { Toaster } from "react-hot-toast"
+import { useThemeStore } from "./store/useThemeStore"
 
 function App() {
   const {authUser,checkAuth,isCheckingAuth}=useAuthStore();
-  
+  const {theme}=useThemeStore()
   useEffect(()=>{
     checkAuth()
   },[checkAuth])
@@ -25,7 +26,7 @@ function App() {
     </div>
   )
 return (
-    <div>
+    <div data-theme={theme}>
       <Toaster />
 
       <Navbar/>
